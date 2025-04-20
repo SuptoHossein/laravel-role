f69534bdd45046bf842a583927ad81a56f956936
SuptoHossein <supto.hossein@gmail.com>
2025-04-21 00:58:51 +0600

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\{
-    PermissionController
+    PermissionController,
+    RoleController
 };
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -19,12 +20,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Permissions routes
     Route::get('/permissions', [PermissionController::class, 'index'])->name('permission.index');
     Route::get('/permissions/create', [PermissionController::class, 'create'])->name('permission.create');
     Route::post('/permissions', [PermissionController::class, 'store'])->name('permission.store');
     Route::get('/permissions/{id}/edit', [PermissionController::class, 'edit'])->name('permission.edit');
     Route::put('/permissions/{id}', [PermissionController::class, 'update'])->name('permission.update');
     Route::delete('/permissions/{id}', [PermissionController::class, 'destroy'])->name('permission.destroy');
+
+    // Roles routes
+    Route::get('/roles', [RoleController::class, 'index'])->name('role.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('role.create');
+    Route::post('/roles', [RoleController::class, 'store'])->name('role.store');
+    Route::get('/roles/{id}/edit', [RoleController::class, 'edit'])->name('role.edit');
+    Route::put('/roles/{id}', [PermissionController::class, 'update'])->name('role.update');
 });
 
 require __DIR__ . '/auth.php';
