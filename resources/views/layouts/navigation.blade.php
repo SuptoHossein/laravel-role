9e24006ee2e92f6f434b6e97bcb916f69d31e5cf
SuptoHossein <supto.hossein@gmail.com>
2025-04-21 00:58:33 +0600

    add new menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,7 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('permission.index')" :active="request()->routeIs('permission.index')">
+                    <x-nav-link :href="route('role.index')" :active="request()->routeIs('role.index')">
                         {{ __('Roles') }}
                     </x-nav-link>
                 </div>
